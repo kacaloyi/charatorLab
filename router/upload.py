92d55f3377b30ba2315b96609e2a97bafd445dd6
kacaloyi <realfreeday@163.com>
2023-05-12 15:00:05 +0800

    上传图片自动修改成正方形小图

--- router/upload.py
+++ router/upload.py
@@ -9,6 +9,7 @@ from conf.config import *
 from controls.control import *
 from utils.auth import *
 from PIL import Image
+import io
 
 from fastapi import APIRouter
 
@@ -30,7 +31,8 @@ def allowed_file(filename):
 async def create_upload_file(file: UploadFile = File(...)):
     if (file.size/1000 )> 500  :
         return {"error": "file is so big"}
-
+    
+    
     if file and allowed_file(file.filename):
         # 这个函数来自werkzeug.utils模块，用于安全处理文件名。它将文件名中的特殊字符替换为下划线，
         # 并删除文件名中的路径信息，以防止路径遍历攻击。
@@ -38,9 +40,10 @@ async def create_upload_file(file: UploadFile = File(...)):
         pathname = os.path.join(UPLOAD_FOLDER, filename)
         hostname = os.path.join(HOST_FOLDER,filename)
 
+        bytes = await file.read()
         try: #如果image能打开，就是图片，改成正方形，并且改尺寸到256,保存。
            
-           with Image.open(file) as image:
+           with Image.open(io.BytesIO(bytes)) as image:
                # 裁剪成正方形
                 width, height = image.size
                 new_size = min(width, height)
@@ -54,7 +57,7 @@ async def create_upload_file(file: UploadFile = File(...)):
            
                 image.save(pathname, 'JPEG')
         except:#不是图片，直接拷贝。
-            bytes = await file.read()
+            
             with open(pathname, 'wb') as  f:
                 f.write(bytes)# imgbytes includes the file format information, so fout.mrite(ingbytes) will save the bytes as image,video or others# fout.write(imgbase64) # if fout.write(imgbase64), the saved file is messy codefout.close()
                 f.close()
