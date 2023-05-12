e130c2a1e2a24c8af46588e2c16968aa757b6b16
kacaloyi <realfreeday@163.com>
2023-05-12 14:38:11 +0800

    修图

--- router/upload.py
+++ router/upload.py
@@ -8,6 +8,7 @@ from models.schemas import *
 from conf.config import *
 from controls.control import *
 from utils.auth import *
+from PIL import Image
 
 from fastapi import APIRouter
 
@@ -34,12 +35,29 @@ async def create_upload_file(file: UploadFile = File(...)):
         # 这个函数来自werkzeug.utils模块，用于安全处理文件名。它将文件名中的特殊字符替换为下划线，
         # 并删除文件名中的路径信息，以防止路径遍历攻击。
         filename = secure_filename(file.filename)
-        bytes = await file.read()
         pathname = os.path.join(UPLOAD_FOLDER, filename)
         hostname = os.path.join(HOST_FOLDER,filename)
-        with open(pathname, 'wb') as  f:
-            f.write(bytes)# imgbytes includes the file format information, so fout.mrite(ingbytes) will save the bytes as image,video or others# fout.write(imgbase64) # if fout.write(imgbase64), the saved file is messy codefout.close()
-            f.close()
+
+        try: #如果image能打开，就是图片，改成正方形，并且改尺寸到256,保存。
+           
+           with Image.open(file) as image:
+               # 裁剪成正方形
+                width, height = image.size
+                new_size = min(width, height)
+                left = (width - new_size) / 2
+                top = (height - new_size) / 2
+                right = (width + new_size) / 2
+                bottom = (height + new_size) / 2
+                image = image.crop((left, top, right, bottom))
+                # 缩放到指定大小
+                image = image.resize((256, 256))
+           
+                image.save(pathname, 'JPEG')
+        except:#不是图片，直接拷贝。
+            bytes = await file.read()
+            with open(pathname, 'wb') as  f:
+                f.write(bytes)# imgbytes includes the file format information, so fout.mrite(ingbytes) will save the bytes as image,video or others# fout.write(imgbase64) # if fout.write(imgbase64), the saved file is messy codefout.close()
+                f.close()
 
         
         return {"hostname":hostname}
